c641c92b62411ee68aa251667b1201cdf8d3da82
zhangyu <1015371461@qq.com>
2024-02-24 00:53:14 +0800

    feat:effect功能

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,8 +1,13 @@
+import { extend } from '../share/index';
 class ReactiveEffect {
+    deps = [];
+    active = true;
+    onStop?: () => void;
     private _fn: () => any;
-
-    constructor(fn) {
+    public scheduler: Function | undefined;
+    constructor(fn, scheduler?: Function) {
         this._fn = fn;
+        this.scheduler = scheduler;
     }
     run() {
         activeEffect = this;
@@ -10,6 +15,21 @@ class ReactiveEffect {
         // 调用runner函数，执行fn并return fn的返回值
         return this._fn();
     }
+    stop() {
+        // 清除依赖
+        if (this.active) {
+            cleanupEffect(this);
+            // 执行stop函数时，如果发现有onStop就执行onStop
+            if (this.onStop) {
+                this.onStop();
+            }
+            this.active = false;
+        }
+    }
+}
+
+function cleanupEffect(effect: ReactiveEffect) {
+    effect.deps.forEach((dep: any) => dep.delete(effect));
 }
 
 // 收集依赖，收集的是所使用的属性的对应的函数
@@ -29,22 +49,43 @@ export function track(target, key) {
         depsMap.set(key, dep);
     }
 
+    if (!activeEffect) return;
+
     dep.add(activeEffect);
+    activeEffect.deps.push(dep);
 }
 
 // 触发更新：将收集起来的key属性对应的所有的fn都执行一遍
 export function trigger(target, key) {
     const depsMap = targetMap.get(target);
     const dep = depsMap.get(key);
-    dep.forEach((effect) => effect.run());
+    for (const effect of dep) {
+        // 判断effect是否有scheduler方法
+        if (effect.scheduler) {
+            effect.scheduler();
+        } else {
+            effect.run();
+        }
+    }
 }
 
 // 执行函数：effect的作用就是执行函数
 let activeEffect;
-export function effect(fn) {
+export function effect(fn, options: any = {}) {
     // fn
-    const _effect = new ReactiveEffect(fn);
+    const _effect = new ReactiveEffect(fn, options.scheduler);
+    // options
+    Object.assign(_effect, options);
+    // extend
+    extend(_effect, options);
+    _effect.onStop = options.onStop;
     _effect.run();
     // 执行effect返回一个runner函数
-    return _effect.run.bind(_effect);
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect;
+    return runner;
+}
+
+export function stop(runner) {
+    runner.effect.stop();
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from '../reactive';
-import { effect } from '../effect';
+import { effect, stop } from '../effect';
 
 describe('effect', () => {
     it('happy path', () => {
@@ -32,5 +32,73 @@ describe('effect', () => {
         const r = runner();
         expect(foo).toBe(12);
         expect(r).toBe('foo');
+    });
+
+    it('scheduler', () => {
+        // 2. effect(fn, { scheduler: fn2 }) => function(runner) => fn => return
+        // 功能：
+        // - 通过effect的第二个参数给定的一个scheduler的fn
+        // - effect 第一次执行的时候还会执行fn
+        // - 当响应式对象set update不会执行fn，而是执行第二个参数scheduler函数
+        // - 如果说当执行runner的时候，会再次的执行fn
+        let dummy;
+        let run: any;
+        const scheduler = jest.fn(() => {
+            run = runner;
+        });
+        const obj = reactive({ foo: 1 });
+        const runner = effect(
+            () => {
+                dummy = obj.foo;
+            },
+            { scheduler }
+        );
+        expect(scheduler).not.toHaveBeenCalled();
+        expect(dummy).toBe(1);
+        // should be called on first trigger
+        obj.foo++;
+        expect(scheduler).toHaveBeenCalledTimes(1);
+        // should not run yet
+        expect(dummy).toBe(1);
+        // manually run
+        run();
+        // should have run
+        expect(dummy).toBe(2);
+    });
+
+    it('stop', () => {
+        // 3. stop 停止特定的effect函数的运行
+        let dummy;
+        const obj = reactive({ prop: 1 });
+        const runner = effect(() => {
+            dummy = obj.prop;
+        });
+        obj.prop = 2;
+        expect(dummy).toBe(2);
+        stop(runner);
+        // should not trigger
+        obj.prop = 3;
+        expect(dummy).toBe(2);
+        // stopped effect should still be manually callable
+        runner();
+        expect(dummy).toBe(3);
+    });
+
+    it('onStop', () => {
+        // 4. 用于在某个effect函数被停止时执行特定的回调函数。
+        // 当使用 stop 函数停止一个effect函数时，可以通过传入 onStop 函数来指定在停止时需要执行的逻辑。
+        const obj = reactive({ prop: 1 });
+        const onStop = jest.fn();
+        let dummy;
+        const runner = effect(
+            () => {
+                dummy = obj.prop;
+            },
+            {
+                onStop,
+            }
+        );
+        stop(runner);
+        expect(onStop).toBeCalledTimes(1);
     });
 });
